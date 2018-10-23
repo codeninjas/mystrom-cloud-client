262b0e5bee8d67175f2ccdb4cf492e16e344a4a4
Michael Ramstein <mr@cyon.ch>
2018-10-23 10:43:23 +0200

    Adds Transport and client layers

---
+++ tests/config.dist.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'url' => 'https://example.com/',
+    'apiKey' => '',
+    'proxy' => [
+        'enabled' => false,
+        'verify-tls' => true,
+        'host' => '127.0.0.1',
+        'post' => 1234
+    ],
+];
